fbe940914e87a67049611b98c42a265e1e7cba59
chen <38033919@qq.com>
2016-09-11 19:25:42 +0800

    Initial commit

---
+++ src/main/java/com/example/ManRepository.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * Created by chen on 16/9/10.
+ */
+public interface ManRepository extends JpaRepository<Man, Long> {
+
+
+}
